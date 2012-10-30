a91ab98e266bc05818f6cb932e8e7a222ee14d90
Satvik Neelakant <nsatvik@gmail.com>
2012-10-31 00:57:34 +0530

    GUI Window Updated!
    A sample GUI Window has been added. Add dependency to the swt project
    and run as a Java Application.
    Enjoy your Power!

--- Encrypter/src/com/jinkchak/MainGUI.java
+++ Encrypter/src/com/jinkchak/MainGUI.java
@@ -1,27 +1,145 @@
 package com.jinkchak;
 
-import java.security.InvalidAlgorithmParameterException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
 
 public class MainGUI {
+	private Shell shell;
 	
-	public MainGUI()
+	private Text EncryptionTextBox;
+	private Text DecryptionTextBox;
+	private Button EncryptButton;
+	private Button EncDecryptCheck;
+	private Label msgLabel;
+	private Label cipherLabel;
+	public MainGUI(Display display)
 	{
+		shell = new Shell(display);
+		initUI();
+		// shell.pack();
+        shell.setLocation(300, 300);
+        shell.setSize(500, 500);
+        shell.setText("Schmidt-Samoa Cryptosystem Demo");
+        shell.setBackgroundImage(new Image(display, "image.jpg"));
+        initListeners();
+        shell.open();
+
+        while (!shell.isDisposed()) {
+          if (!display.readAndDispatch()) {
+            display.sleep();
+          }
+        }
+	}
+	private void initListeners() {
+		EncryptButton.addListener(SWT.MouseDown, new Listener() {
+			
+			@Override
+			public void handleEvent(Event event) {
+				DecryptionTextBox.setText(EncryptionTextBox.getText()+"!");
+				
+			}
+		});
+		
+		EncDecryptCheck.addListener(SWT.CHECK, new Listener() {
+			
+			@Override
+			public void handleEvent(Event event) {
+				DecryptionTextBox.setText(EncryptionTextBox.getText()+" Clicked!");
+				if (EncDecryptCheck.isEnabled())
+				{
+					msgLabel.setText("Cipher: ");
+					cipherLabel.setText("Message: ");
+					EncryptButton.setText("Decrypt");
+				}
+				else
+				{
+					msgLabel.setText("Message: ");
+					cipherLabel.setText("Cipher: ");
+					EncryptButton.setText("Encrypt");
+				}
+				
+			}
+		});
 		
 	}
-	
+	public void initUI() {
+        FormLayout layout = new FormLayout();
+        shell.setLayout(layout);
+        EncryptionTextBox = new Text(shell, SWT.SINGLE | SWT.BORDER);
+        FormData EncTextBoxData = new FormData(100, 100);
+        EncTextBoxData.left = new FormAttachment(25);
+        EncTextBoxData.right = new FormAttachment(75);
+        EncTextBoxData.top = new FormAttachment(20);
+        EncTextBoxData.bottom = new FormAttachment(40);
+        EncryptionTextBox.setLayoutData(EncTextBoxData);
+        
+		DecryptionTextBox = new Text(shell, SWT.SINGLE | SWT.BORDER);
+		 FormData DecTextBoxData = new FormData(100, 100);
+		 DecTextBoxData.left = new FormAttachment(25);
+		 DecTextBoxData.right = new FormAttachment(75);
+		 DecTextBoxData.top = new FormAttachment(50);
+		 DecTextBoxData.bottom = new FormAttachment(70);
+	     DecryptionTextBox.setLayoutData(DecTextBoxData);
+        
+		EncDecryptCheck = new Button(shell, SWT.CHECK);
+
+        EncryptButton = new Button(shell, SWT.PUSH);
+        EncryptButton.setText("Encrypt");
+        FormData encryptButtonData = new FormData(80, 30);
+        encryptButtonData.left = new FormAttachment(25);
+        encryptButtonData.right = new FormAttachment(30);
+        encryptButtonData.top = new FormAttachment(41);
+        encryptButtonData.bottom = new FormAttachment(45);
+        EncryptButton.setLayoutData(encryptButtonData);
+        
+      
+        FormData checkButtonData = new FormData();
+        checkButtonData.left = new FormAttachment(35);
+        checkButtonData.right = new FormAttachment(40);
+        checkButtonData.top = new FormAttachment(41);
+        checkButtonData.bottom = new FormAttachment(44);
+        EncDecryptCheck.setLayoutData(checkButtonData);
+        EncDecryptCheck.setText("Decrypt");
+        
+        msgLabel = new Label(shell,SWT.LEFT);
+        FormData msgLabelData = new FormData(20,20);
+        msgLabelData.left = new FormAttachment(25);
+        msgLabelData.right = new FormAttachment(30);
+        msgLabelData.top = new FormAttachment(18);
+        msgLabelData.bottom = new FormAttachment(20);
+        msgLabel.setText("Message: ");
+        msgLabel.setLayoutData(msgLabelData);
+        
+        cipherLabel = new Label(shell,SWT.LEFT);
+        FormData cipherLabelData = new FormData(30,30);
+        cipherLabelData.left = new FormAttachment(25);
+        cipherLabelData.right = new FormAttachment(30);
+        cipherLabelData.top = new FormAttachment(48);
+        cipherLabelData.bottom = new FormAttachment(50);
+        cipherLabel.setText("Cipher: ");
+        cipherLabel.setLayoutData(cipherLabelData);
+        
+        Label copyLabel = new Label(shell,SWT.BOLD);
+        copyLabel.setText("© 2012 Code Khsetra Inc.\nAll Rights Reserved.\nwww.facebook.com/code-kshetra");
+        
+    }
 	public static void main(String args[])
 	{
-			Schmidt_Samoa_Encryptor ssEncryptor = new Schmidt_Samoa_Encryptor();
-			int [] message = {100,12};
-			int  res = ssEncryptor.encrypt(127);
-//			for (int i=0; i<res.length; ++i)
-//				System.out.print(res[i]);
-			System.out.println();
-			System.out.println("The cipher is "+res);
-			System.out.println("The message is "+ ssEncryptor.decrypt(2337));
-			ssEncryptor.Display();
-//		int [] a = {1,2,3};
-//		System.out.println(a[0]+""+a[1]+""+a[2]);
+			Display display = new Display();
+			new MainGUI(display);
+			display.dispose();
+			
 	}
 
 }
